3555d01cb81cd8f8af6ef04bab5ce4ec13622df4
Jaden Locke <jadenlocke1113@gmail.com>
2020-07-11 18:40:01 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,5 +1,5 @@
 def reverse_each_word(sentence)
-  sentence_array = sentence.split("")
+  sentence_array = sentence.split(" ")
   reversed_words_array = sentence_array.collect do |word|
 	word.reverse
   end
